82823bcc6677922581755422648958fd4d8c8bf0
igor_pont <pontigor11@gmail.com>
2023-01-28 00:07:54 +0300

    Added Formatting_of_lines

---
+++ Python_basics/Formatting_of_lines.py
@@ -0,0 +1,7 @@
+# Форматирование строк
+
+x = '10'
+y = 'cold'
+print('Weather: temperature + {} and it\'s {}'.format(x, y))
+print('Weather: temperature + {1} and it\'s {0}'.format(x, y))
+print(f'Weather: temperature + {x} and it\'s {y}')
